7cccac649888417bba8f94d8a7b11a652e995ebf
Cléophas Fournier <cleophas.fournier@gmail.com>
2025-06-03 09:49:11 +0200

    GCI103 DictionaryItemsUnused add Python tests
    Co-authored-by: DataLabGroupe-CreditAgricole <GITHUB.DATALABGROUPE@CREDIT-AGRICOLE-SA.FR>

--- src/test/resources/checks/DictionaryItemsUnused.py
+++ src/test/resources/checks/DictionaryItemsUnused.py
@@ -28,3 +28,32 @@ for item in my_dict.items():
 entries = []
 for k, v in my_dict.items():
     entries.append((k, v))  
+
+for key, value in my_dict.items():  # Noncompliant {{Use dict.keys() or dict.values() instead of dict.items() when only one part of the key-value pair is used}}
+    do_something_with(key)
+
+for k, v in my_dict.items():  # Noncompliant {{Use dict.keys() or dict.values() instead of dict.items() when only one part of the key-value pair is used}}
+    do_something_with(v)
+
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
+
+for k, v in my_dict.items():
+    some_list.append((k, v))
+
+for k, v in my_dict.items():  # Noncompliant {{Use dict.keys() or dict.values() instead of dict.items() when only one part of the key-value pair is used}}
+    used_keys.append(k)
+
+if True:
+    for k, v in my_dict.items():
+        print(k)
+        print(v)
+
+copied_dict = dict(my_dict.items())
+
+
+for i, (k, v) in enumerate(my_dict.items()):
+    print(i, k, v)
+
+
+{(k, v) for k, v in my_dict.items()}
