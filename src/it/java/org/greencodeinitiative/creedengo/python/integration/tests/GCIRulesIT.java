48a3597de1f739e813f89e6770203469ae07f5d6
Cléophas Fournier <cleophas.fournier@gmail.com>
2025-07-21 11:39:12 +0200

    add integration test/ waiting for release

--- src/it/java/org/greencodeinitiative/creedengo/python/integration/tests/GCIRulesIT.java
+++ src/it/java/org/greencodeinitiative/creedengo/python/integration/tests/GCIRulesIT.java
@@ -273,4 +273,20 @@ class GCIRulesIT extends GCIRulesBase {
 
     }
 
+    @Test 
+    void testGCI100() {
+
+        String filePath = "src/disableGradientForModelEval.py";
+        String ruleId = "creedengo-python:GCI100";
+        String ruleMsg = "PyTorch : Disable gradient computation when evaluating a model to save memory and computation time";
+        int[] startLines = new int[]{
+            19, 29, 38
+        };
+        int[] endLines = new int[]{
+            19, 29, 38
+        };
+
+        checkIssuesForFile(filePath, ruleId, ruleMsg, startLines, endLines, SEVERITY, TYPE, EFFORT_1h);
+
+    }
 }
