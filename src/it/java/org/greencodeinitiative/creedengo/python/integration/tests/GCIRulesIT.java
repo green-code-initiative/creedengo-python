1a049990672fa1838dc8a256b12d5deee0ace6e8
Cléophas Fournier <cleophas.fournier@gmail.com>
2025-07-21 11:49:54 +0200

    GCI101 AvoidConvBiasBeforeBatchNorm update: add integrations/real tests
    Co-authored-by: DataLabGroupe-CreditAgricole <GITHUB.DATALABGROUPE@CREDIT-AGRICOLE-SA.FR>

--- src/it/java/org/greencodeinitiative/creedengo/python/integration/tests/GCIRulesIT.java
+++ src/it/java/org/greencodeinitiative/creedengo/python/integration/tests/GCIRulesIT.java
@@ -273,4 +273,20 @@ class GCIRulesIT extends GCIRulesBase {
 
     }
 
+    @Test
+    void testGCI101(){
+        String filePath = "src/avoidConvBiasBeforeBatchNorm.py";
+        String ruleId = "creedengo-python:GCI101";
+        String ruleMsg = "Remove bias for convolutions before batch norm layers to save time and memory.";
+        int[] startLines = new int[]{
+            49, 71, 115, 136, 156, 178
+        };
+        int[] endLines = new int[]{
+            49, 71, 115, 136, 156, 178
+        };
+
+        checkIssuesForFile(filePath, ruleId, ruleMsg, startLines, endLines, SEVERITY, TYPE, EFFORT_10MIN);
+
+    }
+
 }
